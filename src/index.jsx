f4c072f6807fe5777601a95b1e4e82c3dbc54a93
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-01-11 22:18:18 +0100

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
+
+// Import statement to indicate that you need to bundle './index.scss'
+import './index.scss';
+
+// Main component (will eventually use all the others)
+// MainView component is imported and used on the page
+const App = () => {
+  return <MainView />;
+};
+
+// Finds the root of your app
+const container = document.querySelector('#root');
+const root = createRoot(container);
+
+// Говорит РЕАКТУ воспроизводить апп в корневом элементе ДОМ (определен выше)
+root.render(<App />);
